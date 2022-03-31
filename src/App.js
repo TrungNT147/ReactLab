4a76cfcc40ceac75923d02d79e300f19a7328d17
Trung <trungntfx14921@funix.edu.vn>
2022-03-31 16:59:55 +0700

    Component part 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import React, { Component } from 'react'
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes' 
 import './App.css';
 
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes : DISHES
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +23,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
